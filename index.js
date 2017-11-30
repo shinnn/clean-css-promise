a66211fe6af4f4edb6412b9c2302585406cae9a6
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-30 20:01:28 +0900

    update dependencies and devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -9,68 +9,64 @@ const REBASE_TO_ERROR = 'Expected `rebaseTo` option to be a string or undefined'
 const createLine = (msg, error, index) => `${msg}\n  ${index + 1}. ${error}`;
 
 module.exports = class CleanCssPromise extends CleanCss {
-  constructor(options) {
-    if (options !== undefined && options !== null) {
-      if (typeof options !== 'object') {
-        throw new TypeError(`${CONSTRUCTOR_ERROR}, but got a non-object value ${
-          inspect(options)
-        } instead.`);
-      }
+	constructor(options) {
+		if (options !== undefined && options !== null) {
+			if (typeof options !== 'object') {
+				throw new TypeError(`${CONSTRUCTOR_ERROR}, but got a non-object value ${
+					inspect(options)
+				} instead.`);
+			}
 
-      if (Array.isArray(options)) {
-        throw new TypeError(`${CONSTRUCTOR_ERROR}, but got an array ${inspect(options)} instead.`);
-      }
+			if (Array.isArray(options)) {
+				throw new TypeError(`${CONSTRUCTOR_ERROR}, but got an array ${inspect(options)} instead.`);
+			}
 
-      if (options.returnPromise === false) {
-        throw new Error('clean-css-promise requires `returnPromise` option to be enabled.');
-      }
+			if (options.returnPromise === false) {
+				throw new Error('clean-css-promise requires `returnPromise` option to be enabled.');
+			}
 
-      if (options.returnPromise !== undefined) {
-        throw new Error(
-          'clean-css-promise enables `returnPromise` option by default, so you dont\'t need to ' +
-          'pass any values to that option. But ' +
-          inspect(options.returnPromise) +
-          ' is provided.'
-        );
-      }
+			if (options.returnPromise !== undefined) {
+				throw new Error(`${'clean-css-promise enables `returnPromise` option by default, so you dont\'t need to ' +
+          'pass any values to that option. But '}${
+					inspect(options.returnPromise)
+				} is provided.`);
+			}
 
-      if (options.rebaseTo !== undefined && typeof options.rebaseTo !== 'string') {
-        throw new TypeError(`${REBASE_TO_ERROR}, but got ${inspect(options.rebaseTo)}.${
-          options.rebaseTo === null || options.rebaseTo === false ?
-          ' If you want to disable `rebaseTo` option, do not pass any values to `rebaseTo`.' :
-          ''
-        }`);
-      }
-    } else {
-      options = {};
-    }
+			if (options.rebaseTo !== undefined && typeof options.rebaseTo !== 'string') {
+				throw new TypeError(`${REBASE_TO_ERROR}, but got ${inspect(options.rebaseTo)}.${
+					options.rebaseTo === null || options.rebaseTo === false ?
+						' If you want to disable `rebaseTo` option, do not pass any values to `rebaseTo`.' :
+						''
+				}`);
+			}
+		} else {
+			options = {};
+		}
 
-    super(options);
-  }
+		super(options);
+	}
 
-  minify(source) {
-    return new Promise((resolve, reject) => {
-      super.minify(source, (unusedArg, result) => {
-        const errors = result.errors.concat(result.warnings);
-        const errorCount = errors.length;
+	minify(source) {
+		return new Promise((resolve, reject) => {
+			super.minify(source, (unusedArg, result) => {
+				const errors = result.errors.concat(result.warnings);
+				const errorCount = errors.length;
 
-        if (errorCount !== 0) {
-          const errorMessage = errorCount !== 1 ? errors.reduce(
-            createLine,
-            `${errors.length} errors found while optimizing CSS with clean-css:`
-          ) : `An error found while optimizing CSS with clean-css:\n  * ${errors[0]}`;
+				if (errorCount !== 0) {
+					const errorMessage = errorCount !== 1 ? errors.reduce(
+						createLine,
+						`${errors.length} errors found while optimizing CSS with clean-css:`
+					) : `An error found while optimizing CSS with clean-css:\n  * ${errors[0]}`;
 
-          reject(new Error(
-            errorMessage +
-            `\n\nclean-css dangerously ignores ${
-              errorCount === 1 ? 'this error' : 'these errors'
-            } but clean-css-promise doesn't, because it's much more reasonable to update the CSS` +
-            ' to fix all problems than to pretend that you didn\'t see the errors.'
-          ));
-        }
+					reject(new Error(`${errorMessage
+					}\n\nclean-css dangerously ignores ${
+						errorCount === 1 ? 'this error' : 'these errors'
+					} but clean-css-promise doesn't, because it's much more reasonable to update the CSS` +
+            ' to fix all problems than to pretend that you didn\'t see the errors.'));
+				}
 
-        resolve(result);
-      });
-    });
-  }
+				resolve(result);
+			});
+		});
+	}
 };
--- test.js
+++ test.js
@@ -10,79 +10,79 @@ const expectedMsg = `3 errors found while optimizing CSS with clean-css:
   3. Skipping remote @import of "https://exmaple.com" as resource is not allowed.`;
 
 test('CleanCssPromise class', t => {
-  t.deepEqual(
-    new CleanCssPromise().options,
-    new CleanCss().options,
-    'should be an inheritance of CleanCSS class.'
-  );
+	t.deepEqual(
+		new CleanCssPromise().options,
+		new CleanCss().options,
+		'should be an inheritance of CleanCSS class.'
+	);
 
-  t.throws(
-    () => new CleanCssPromise(1),
-    /^TypeError.*Expected an object to specify clean-css options, but got a non-object value 1 instead\./,
-    'should throw an error when it takes a non-object argument.'
-  );
+	t.throws(
+		() => new CleanCssPromise(1),
+		/^TypeError.*Expected an object to specify clean-css options, but got a non-object value 1 instead\./,
+		'should throw an error when it takes a non-object argument.'
+	);
 
-  t.throws(
-    () => new CleanCssPromise([]),
-    /^TypeError.*Expected an object to specify clean-css options, but got an array \[] instead\./,
-    'should throw an error when it takes an array.'
-  );
+	t.throws(
+		() => new CleanCssPromise([]),
+		/^TypeError.*Expected an object to specify clean-css options, but got an array \[] instead\./,
+		'should throw an error when it takes an array.'
+	);
 
-  t.throws(
-    () => new CleanCssPromise({returnPromise: false}),
-    /^Error.*clean-css-promise requires `returnPromise` option to be enabled\./,
-    'should throw an error when `returnPromise` option is disabled.'
-  );
+	t.throws(
+		() => new CleanCssPromise({returnPromise: false}),
+		/^Error.*clean-css-promise requires `returnPromise` option to be enabled\./,
+		'should throw an error when `returnPromise` option is disabled.'
+	);
 
-  t.throws(
-    () => new CleanCssPromise({returnPromise: true}),
-    /^Error.*so you dont't need to pass any values to that option\. But true is provided\./,
-    'should throw an error when `returnPromise` option receives any value.'
-  );
+	t.throws(
+		() => new CleanCssPromise({returnPromise: true}),
+		/^Error.*so you dont't need to pass any values to that option\. But true is provided\./,
+		'should throw an error when `returnPromise` option receives any value.'
+	);
 
-  t.throws(
-    () => new CleanCssPromise({rebaseTo: new Set()}),
-    /^TypeError.*Expected `rebaseTo` option to be a string or undefined, but got Set {}\./,
-    'should throw an error when `rebaseTo` option is not a string.'
-  );
+	t.throws(
+		() => new CleanCssPromise({rebaseTo: new Set()}),
+		/^TypeError.*Expected `rebaseTo` option to be a string or undefined, but got Set {}\./,
+		'should throw an error when `rebaseTo` option is not a string.'
+	);
 
-  t.throws(
-    () => new CleanCssPromise({rebaseTo: false}),
-    /^TypeError.*If you want to disable `rebaseTo` option, do not pass any values to `rebaseTo`\./,
-    'should throw an error when `rebaseTo` option is explicitly disabled.'
-  );
+	t.throws(
+		() => new CleanCssPromise({rebaseTo: false}),
+		/^TypeError.*If you want to disable `rebaseTo` option, do not pass any values to `rebaseTo`\./,
+		'should throw an error when `rebaseTo` option is explicitly disabled.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('CleanCssPromise#minify()', t => {
-  t.plan(4);
+	t.plan(4);
 
-  new CleanCssPromise().minify('a { color: #FF0000 }').then(result => {
-    t.strictEqual(result.styles, 'a{color:red}', 'should minify CSS.');
-  }).catch(t.fail);
+	new CleanCssPromise().minify('a { color: #FF0000 }').then(result => {
+		t.equal(result.styles, 'a{color:red}', 'should minify CSS.');
+	}).catch(t.fail);
 
-  new CleanCssPromise({
-    compatibility: {
-      properties: {
-        zeroUnits: false
-      }
-    }
-  }).minify('b {font: 0px}').then(result => {
-    t.strictEqual(result.styles, 'b{font:0px}', 'should support clean-css options.');
-  }).catch(t.fail);
+	new CleanCssPromise({
+		compatibility: {
+			properties: {
+				zeroUnits: false
+			}
+		}
+	}).minify('b {font: 0px}').then(result => {
+		t.equal(result.styles, 'b{font:0px}', 'should support clean-css options.');
+	}).catch(t.fail);
 
-  new CleanCssPromise(null).minify('@import /foo;').then(t.fail, ({message}) => {
-    t.ok(
-      message.startsWith('An error found while optimizing CSS with clean-css'),
-      'should fail when an error occurs while optimizing CSS.'
-    );
-  });
+	new CleanCssPromise(null).minify('@import /foo;').then(t.fail, ({message}) => {
+		t.ok(
+			message.startsWith('An error found while optimizing CSS with clean-css'),
+			'should fail when an error occurs while optimizing CSS.'
+		);
+	});
 
-  new CleanCssPromise().minify('@import url(https://exmaple.com);a}').then(t.fail, ({message}) => {
-    t.ok(
-      message.startsWith(expectedMsg),
-      'should fail when multiple errors occur while optimizing CSS.'
-    );
-  });
+	new CleanCssPromise().minify('@import url(https://exmaple.com);a}').then(t.fail, ({message}) => {
+		t.ok(
+			message.startsWith(expectedMsg),
+			'should fail when multiple errors occur while optimizing CSS.'
+		);
+	});
 });
